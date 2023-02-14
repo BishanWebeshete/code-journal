3aea1f58d129c64f1f99ae672e92b4fd024eda98
Bishan Webeshete <webesheteb@gmail.com>
2023-02-14 00:15:04 +0000

    created input and submit event listeners to change photo and save data into object

--- js/main.js
+++ js/main.js
@@ -1,5 +1,21 @@
-var photoUrlInput = document.querySelector('#photourl');
-var img = document.querySelector('img');
-photoUrlInput.addEventListener('input', function (event) {
-  img.setAttribute('src', event.target.value);
+var $photoUrlInput = document.querySelector('#photourl');
+var $img = document.querySelector('img');
+var $form = document.querySelector('form');
+
+$photoUrlInput.addEventListener('input', function (event) {
+  $img.setAttribute('src', event.target.value);
+});
+
+$form.addEventListener('submit', function (event) {
+  event.preventDefault();
+  var $formInfo = {
+    title: $form.elements.title.value,
+    photoUrl: $form.elements.photourl.value,
+    notes: $form.elements.textarea.value,
+    entryId: data.nextEntryId
+  };
+  data.nextEntryId++;
+  data.entries.push($formInfo);
+  $img.setAttribute('src', 'images/placeholder-image-sqaure.jpg');
+  $form.reset();
 });
