4e9f20eb8cff7803dcae904b7d5a7363fdf680d0
Bishan Webeshete <webesheteb@gmail.com>
2023-02-14 20:14:11 +0000

    added event listener for DOMContentLoaded to append the dom tree

--- js/main.js
+++ js/main.js
@@ -19,3 +19,41 @@ $form.addEventListener('submit', function (event) {
   $img.setAttribute('src', 'images/placeholder-image-sqaure.jpg');
   $form.reset();
 });
+
+function renderEntry(entry) {
+  var $liElement = document.createElement('li');
+
+  var $rowDiv = document.createElement('div');
+  $rowDiv.className = 'row';
+  $liElement.appendChild($rowDiv);
+
+  var $firstColumnDiv = document.createElement('div');
+  $firstColumnDiv.className = 'column-half';
+  $rowDiv.appendChild($firstColumnDiv);
+
+  var $imageElement = document.createElement('img');
+  $imageElement.setAttribute('src', entry.photoUrl);
+  $firstColumnDiv.appendChild($imageElement);
+
+  var $secondColumnDiv = document.createElement('div');
+  $secondColumnDiv.className = 'column-half';
+  $rowDiv.appendChild($secondColumnDiv);
+
+  var $h2Element = document.createElement('h2');
+  $h2Element.textContent = entry.title;
+  $secondColumnDiv.appendChild($h2Element);
+
+  var $pElement = document.createElement('p');
+  $pElement.textContent = entry.notes;
+  $secondColumnDiv.appendChild($pElement);
+
+  return $liElement;
+}
+
+var $ul = document.querySelector('ul');
+
+document.addEventListener('DOMContentLoaded', function (event) {
+  data.entries.forEach(entry => {
+    $ul.append(renderEntry(entry));
+  });
+});
